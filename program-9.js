4a69e2c8e59db2e79babf71dce14366cbea77522
vatsalmaru02 <vatsalmaru02@gmail.com>
2025-11-11 15:09:06 +0530

    JS test-3 with changes

--- program-9.js
+++ program-9.js
@@ -1,17 +1,18 @@
 // Write a JavaScript program to generate all combinations of a string.
 
 const generateCombinations = (str) => {
-  const combinations = [];
+  const combinations = new Set();
   const length = str.length;
-
   for (let i = 0; i < length; i++) {
     for (let j = i + 1; j <= length; j++) {
-      combinations.push(str.slice(i, j).toLowerCase());
+      combinations.add(str.slice(i, j).toLowerCase());
     }
   }
-  return combinations;
+  return Array.from(combinations);
 };
 
-console.log(generateCombinations("Dog"));
+console.log(generateCombinations("Elephant"));
+
+
 // Input: Dog
 // Output: [ 'd', 'do', 'dog', 'o', 'og', 'g' ]
